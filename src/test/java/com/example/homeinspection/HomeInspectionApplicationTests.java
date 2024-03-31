5066d2a212ba4b090d4ac73a6128f11581826541
Andrei Likhoded <liberty102and8131186@gmail.com>
2024-03-31 15:18:49 +0300

    first commit

---
+++ src/test/java/com/example/homeinspection/HomeInspectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeinspection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeInspectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
